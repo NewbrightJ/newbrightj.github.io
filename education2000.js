45a59482af3d33a76d7d56426ddc47771661b501
Evan Bates <evanbates22@gmail.com>
2018-04-10 17:13:38 -0400

    update names

--- education2000.js
+++ education2000.js
@@ -1,92 +1,92 @@
 var education2000 = [
     {
-      "Geographical Area": "Congressional District 1 (110th Congress)",
-      "high school": 80.1,
-      "bachelor's": 22.3
+      "Geographical_Area": "Congressional District 1 (110th Congress)",
+      "high_school": 80.1,
+      "bachelors": 22.3
     },
     {
-      "Geographical Area": "Congressional District 2 (110th Congress)",
-      "high school": 83.3,
-      "bachelor's": 29
+      "Geographical_Area": "Congressional District 2 (110th Congress)",
+      "high_school": 83.3,
+      "bachelors": 29
     },
     {
-      "Geographical Area": "Congressional District 3 (110th Congress)",
-      "high school": 83.8,
-      "bachelor's": 22.7
+      "Geographical_Area": "Congressional District 3 (110th Congress)",
+      "high_school": 83.8,
+      "bachelors": 22.7
     },
     {
-      "Geographical Area": "Congressional District 4 (110th Congress)",
-      "high school": 82.4,
-      "bachelor's": 13.1
+      "Geographical_Area": "Congressional District 4 (110th Congress)",
+      "high_school": 82.4,
+      "bachelors": 13.1
     },
     {
-      "Geographical Area": "Congressional District 5 (110th Congress)",
-      "high school": 84.2,
-      "bachelor's": 14.6
+      "Geographical_Area": "Congressional District 5 (110th Congress)",
+      "high_school": 84.2,
+      "bachelors": 14.6
     },
     {
-      "Geographical Area": "Congressional District 6 (110th Congress)",
-      "high school": 81.2,
-      "bachelor's": 14.2
+      "Geographical_Area": "Congressional District 6 (110th Congress)",
+      "high_school": 81.2,
+      "bachelors": 14.2
     },
     {
-      "Geographical Area": "Congressional District 7 (110th Congress)",
-      "high school": 83.5,
-      "bachelor's": 18.7
+      "Geographical_Area": "Congressional District 7 (110th Congress)",
+      "high_school": 83.5,
+      "bachelors": 18.7
     },
     {
-      "Geographical Area": "Congressional District 8 (110th Congress)",
-      "high school": 82.5,
-      "bachelor's": 18.7
+      "Geographical_Area": "Congressional District 8 (110th Congress)",
+      "high_school": 82.5,
+      "bachelors": 18.7
     },
     {
-      "Geographical Area": "Congressional District 9 (110th Congress)",
-      "high school": 83.3,
-      "bachelor's": 19.8
+      "Geographical_Area": "Congressional District 9 (110th Congress)",
+      "high_school": 83.3,
+      "bachelors": 19.8
     },
     {
-      "Geographical Area": "Congressional District 10 (110th Congress)",
-      "high school": 82.7,
-      "bachelor's": 23.3
+      "Geographical_Area": "Congressional District 10 (110th Congress)",
+      "high_school": 82.7,
+      "bachelors": 23.3
     },
     {
-      "Geographical Area": "Congressional District 11 (110th Congress)",
-      "high school": 78.2,
-      "bachelor's": 23.4
+      "Geographical_Area": "Congressional District 11 (110th Congress)",
+      "high_school": 78.2,
+      "bachelors": 23.4
     },
     {
-      "Geographical Area": "Congressional District 12 (110th Congress)",
-      "high school": 87.5,
-      "bachelor's": 32.1
+      "Geographical_Area": "Congressional District 12 (110th Congress)",
+      "high_school": 87.5,
+      "bachelors": 32.1
     },
     {
-      "Geographical Area": "Congressional District 13 (110th Congress)",
-      "high school": 84.6,
-      "bachelor's": 22.3
+      "Geographical_Area": "Congressional District 13 (110th Congress)",
+      "high_school": 84.6,
+      "bachelors": 22.3
     },
     {
-      "Geographical Area": "Congressional District 14 (110th Congress)",
-      "high school": 87.2,
-      "bachelor's": 27.1
+      "Geographical_Area": "Congressional District 14 (110th Congress)",
+      "high_school": 87.2,
+      "bachelors": 27.1
     },
     {
-      "Geographical Area": "Congressional District 15 (110th Congress)",
-      "high school": 85.5,
-      "bachelor's": 32.1
+      "Geographical_Area": "Congressional District 15 (110th Congress)",
+      "high_school": 85.5,
+      "bachelors": 32.1
     },
     {
-      "Geographical Area": "Congressional District 16 (110th Congress)",
-      "high school": 83.9,
-      "bachelor's": 19.2
+      "Geographical_Area": "Congressional District 16 (110th Congress)",
+      "high_school": 83.9,
+      "bachelors": 19.2
     },
     {
-      "Geographical Area": "Congressional District 17 (110th Congress)",
-      "high school": 82.2,
-      "bachelor's": 15.8
+      "Geographical_Area": "Congressional District 17 (110th Congress)",
+      "high_school": 82.2,
+      "bachelors": 15.8
     },
     {
-      "Geographical Area": "Congressional District 18 (110th Congress)",
-      "high school": 77.5,
-      "bachelor's": 11.3
+      "Geographical_Area": "Congressional District 18 (110th Congress)",
+      "high_school": 77.5,
+      "bachelors": 11.3
     }
   ];
