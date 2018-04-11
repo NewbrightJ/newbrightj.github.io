3c7aa534ba929d0441853fd6249dcd6776fca424
Evan Bates <evanbates22@gmail.com>
2018-04-10 21:00:58 -0400

    changed names

--- education2013.js
+++ education2013.js
@@ -1,82 +1,82 @@
 var education2013 = [
   {
-    "Geographic Area": "Ohio - Congressional District 1 (113th Congress)",
-    "High School": 87.9,
-    "Bachelor's": 30.1
+    "Geographical_Area": "Ohio - Congressional District 1 (113th Congress)",
+    "high_school": 87.9,
+    "bachelors": 30.1
   },
   {
-    "Geographic Area": "Ohio - Congressional District 2 (113th Congress)",
-    "High School": 87.1,
-    "Bachelor's": 28.4
+    "Geographical_Area": "Ohio - Congressional District 2 (113th Congress)",
+    "high_school": 87.1,
+    "bachelors": 28.4
   },
   {
-    "Geographic Area": "Ohio - Congressional District 3 (113th Congress)",
-    "High School": 85.9,
-    "Bachelor's": 25.5
+    "Geographical_Area": "Ohio - Congressional District 3 (113th Congress)",
+    "high_school": 85.9,
+    "bachelors": 25.5
   },
   {
-    "Geographic Area": "Ohio - Congressional District 4 (113th Congress)",
-    "High School": 88.1,
-    "Bachelor's": 15.8
+    "Geographical_Area": "Ohio - Congressional District 4 (113th Congress)",
+    "high_school": 88.1,
+    "bachelors": 15.8
   },
   {
-    "Geographic Area": "Ohio - Congressional District 5 (113th Congress)",
-    "High School": 91,
-    "Bachelor's": 23.4
+    "Geographical_Area": "Ohio - Congressional District 5 (113th Congress)",
+    "high_school": 91,
+    "bachelors": 23.4
   },
   {
-    "Geographic Area": "Ohio - Congressional District 6 (113th Congress)",
-    "High School": 86,
-    "Bachelor's": 14.3
+    "Geographical_Area": "Ohio - Congressional District 6 (113th Congress)",
+    "high_school": 86,
+    "bachelors": 14.3
   },
   {
-    "Geographic Area": "Ohio - Congressional District 7 (113th Congress)",
-    "High School": 86.3,
-    "Bachelor's": 18
+    "Geographical_Area": "Ohio - Congressional District 7 (113th Congress)",
+    "high_school": 86.3,
+    "bachelors": 18
   },
   {
-    "Geographic Area": "Ohio - Congressional District 8 (113th Congress)",
-    "High School": 87.7,
-    "Bachelor's": 21.6
+    "Geographical_Area": "Ohio - Congressional District 8 (113th Congress)",
+    "high_school": 87.7,
+    "bachelors": 21.6
   },
   {
-    "Geographic Area": "Ohio - Congressional District 9 (113th Congress)",
-    "High School": 85.5,
-    "Bachelor's": 20
+    "Geographical_Area": "Ohio - Congressional District 9 (113th Congress)",
+    "high_school": 85.5,
+    "bachelors": 20
   },
   {
-    "Geographic Area": "Ohio - Congressional District 10 (113th Congress)",
-    "High School": 88.8,
-    "Bachelor's": 26.4
+    "Geographical_Area": "Ohio - Congressional District 10 (113th Congress)",
+    "high_school": 88.8,
+    "bachelors": 26.4
   },
   {
-    "Geographic Area": "Ohio - Congressional District 11 (113th Congress)",
-    "High School": 83.8,
-    "Bachelor's": 25.1
+    "Geographical_Area": "Ohio - Congressional District 11 (113th Congress)",
+    "high_school": 83.8,
+    "bachelors": 25.1
   },
   {
-    "Geographic Area": "Ohio - Congressional District 12 (113th Congress)",
-    "High School": 92,
-    "Bachelor's": 37.3
+    "Geographical_Area": "Ohio - Congressional District 12 (113th Congress)",
+    "high_school": 92,
+    "bachelors": 37.3
   },
   {
-    "Geographic Area": "Ohio - Congressional District 13 (113th Congress)",
-    "High School": 87.9,
-    "Bachelor's": 20
+    "Geographical_Area": "Ohio - Congressional District 13 (113th Congress)",
+    "high_school": 87.9,
+    "bachelors": 20
   },
   {
-    "Geographic Area": "Ohio - Congressional District 14 (113th Congress)",
-    "High School": 91.1,
-    "Bachelor's": 31.4
+    "Geographical_Area": "Ohio - Congressional District 14 (113th Congress)",
+    "high_school": 91.1,
+    "bachelors": 31.4
   },
   {
-    "Geographic Area": "Ohio - Congressional District 15 (113th Congress)",
-    "High School": 89.5,
-    "Bachelor's": 27.9
+    "Geographical_Area": "Ohio - Congressional District 15 (113th Congress)",
+    "high_school": 89.5,
+    "bachelors": 27.9
   },
   {
-    "Geographic Area": "Ohio - Congressional District 16 (113th Congress)",
-    "High School": 92.1,
-    "Bachelor's": 30.7
+    "Geographical_Area": "Ohio - Congressional District 16 (113th Congress)",
+    "high_school": 92.1,
+    "bachelors": 30.7
   }
 ];
